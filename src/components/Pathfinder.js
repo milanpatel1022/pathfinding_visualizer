88c730a933254560a36d640802ab8e4e3da35cf8
Milan Patel <patelmj1@uci.edu>
2022-07-06 16:33:49 -0700

    used HTML table to display grid and added styling

--- src/components/Pathfinder.js
+++ src/components/Pathfinder.js
@@ -8,17 +8,17 @@ class Pathfinder extends Component {
     constructor() {
         super();
         this.state = {
-            grid: [],
+            grid: [], //our 2d array/grid
         };
     }
 
     //create the grid as soon as this component is inserted into the DOM & update state.grid (which is initially an empty array)
     componentDidMount(){
-        const newGrid = this.createGrid(25, 50);
+        const newGrid = this.createGrid(25, 40);
         this.setState({grid: newGrid}, () => console.log(this.state.grid))
     }
 
-    //create 2D array/grid of Nodes
+    //create 2D array/grid (each cell is an object containing info.)
     createGrid = (ROWS, COLS) => {
         const grid = []
 
@@ -26,7 +26,7 @@ class Pathfinder extends Component {
             const curRow = []
 
             for (let col = 0; col < COLS; col++){
-                const node = this.createNode(row, col)
+                const node = this.createCell(row, col)
                 curRow.push(node)
             }
 
@@ -36,8 +36,8 @@ class Pathfinder extends Component {
         return grid
     };
 
-    //each Node stores: its row and col
-    createNode = (row, col) => {
+    //each cell stores: its row and col (for now)
+    createCell = (row, col) => {
         return {
             row: row,
             col: col
@@ -45,21 +45,28 @@ class Pathfinder extends Component {
     }
 
     render() {
+        //get the current state of our 2d array of objects
         const grid = this.state.grid;
 
         return (
             <div className = "grid">
-                {grid.map((row, rowIdx) => {
-                    return (
-                        <div key={rowIdx}>
-                            {row.map((node, nodeIdx) => {
-                                return (
-                                    <Node key={nodeIdx} row={row} col={nodeIdx}/>
-                                )
-                            })}
-                        </div>
-                    );
-                })}
+                <table>
+                    <tbody>
+                        {grid.map((row, rowIdx) => {
+                            return (
+                                <tr key={rowIdx}>
+                                    {row.map((node, nodeIdx) => {
+                                        return (
+                                            <td key={nodeIdx}>
+                                                <Node row={row} col={nodeIdx}/>
+                                            </td>
+                                        )
+                                    })}
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
             </div>
         )
     }
