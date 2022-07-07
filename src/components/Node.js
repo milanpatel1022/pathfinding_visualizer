1eb004e73b7926597a3f8e753786fbeb25147aef
Milan Patel <patelmj1@uci.edu>
2022-07-06 17:37:15 -0700

    identified and styled starting and end points. currently hard coded, but will allow user to move these points around in future.

--- src/components/Node.js
+++ src/components/Node.js
@@ -3,8 +3,15 @@ import './Node.css'
 
 class Node extends Component {
   render() {
+    const {row, col, isStart, isEnd} = this.props;
+
+    //give start and end node a class name so we can style them accordingly.
+    const nodeClass = isStart ? 'startPoint' : isEnd ? 'endPoint' : '';
+    console.log(nodeClass)
+
     return (
-      <div className="node"></div>
+      //each node has two classes. node class & an extra one to identify if it is a startPoint, endPoint or neither
+      <div className={`node ${nodeClass}`}></div>
     )
   }
 }
--- src/components/Pathfinder.js
+++ src/components/Pathfinder.js
@@ -3,6 +3,14 @@ import Node from './Node'
 
 import './Pathfinder.css'
 
+const rows = 25
+const cols = 40
+
+const START_ROW = 0
+const START_COL = 0
+const END_ROW = rows-1
+const END_COL = cols-1
+
 class Pathfinder extends Component {
 
     constructor() {
@@ -14,18 +22,18 @@ class Pathfinder extends Component {
 
     //create the grid as soon as this component is inserted into the DOM & update state.grid (which is initially an empty array)
     componentDidMount(){
-        const newGrid = this.createGrid(25, 40);
+        const newGrid = this.createGrid();
         this.setState({grid: newGrid}, () => console.log(this.state.grid))
     }
 
     //create 2D array/grid (each cell is an object containing info.)
-    createGrid = (ROWS, COLS) => {
+    createGrid = () => {
         const grid = []
 
-        for (let row = 0; row < ROWS; row++){
+        for (let row = 0; row < rows; row++){
             const curRow = []
 
-            for (let col = 0; col < COLS; col++){
+            for (let col = 0; col < cols; col++){
                 const node = this.createCell(row, col)
                 curRow.push(node)
             }
@@ -36,11 +44,13 @@ class Pathfinder extends Component {
         return grid
     };
 
-    //each cell stores: its row and col (for now)
+    //each cell stores: its row and col & if it is the starting/end point
     createCell = (row, col) => {
         return {
             row: row,
-            col: col
+            col: col,
+            isStart: row === START_ROW && col === START_COL,
+            isEnd: row === END_ROW && col === END_COL,
         }
     }
 
@@ -58,7 +68,7 @@ class Pathfinder extends Component {
                                     {row.map((node, nodeIdx) => {
                                         return (
                                             <td key={nodeIdx}>
-                                                <Node row={row} col={nodeIdx}/>
+                                                <Node row={row} col={nodeIdx} isStart={node.isStart} isEnd={node.isEnd}/>
                                             </td>
                                         )
                                     })}
